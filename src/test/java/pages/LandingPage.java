abaf8ff518372777b5a073e08160580cbb28e984
Umutayb <umutaybora@gmail.com>
2021-12-13 12:20:16 +0100

    Updated Mobileib

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -6,6 +6,7 @@ import io.appium.java_client.pagefactory.*;
 import utils.Utilities;
 
 public class LandingPage extends Utilities {
+
     @AndroidFindBy(id = "com.allandroidprojects.getirsample:id/btn_next")
     @iOSBy(id = "")
     public MobileElement nextButton;
