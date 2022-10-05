664bdff719df7ceb6f7f4b6a2f7d17e22feb2ddb
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-10-05 18:46:28 +0200

    Improved TestRunner.java
    Cleaned screenshots.

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -2,7 +2,12 @@ import common.ShutdownSequence;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import utils.Printer;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -12,6 +17,24 @@ import org.junit.runner.RunWith;
         publish = true
 )
 public class TestRunner {
+
+    @BeforeClass
+    public static void initialSequence(){
+        File dir = new File("screenshots");
+        File[] screenshots = dir.listFiles();
+        assert screenshots != null;
+        String mediaType;
+        if(screenshots.length>0) for (File screenshot : screenshots) {
+            try {mediaType = Files.probeContentType(screenshot.toPath());}
+            catch (IOException e) {throw new RuntimeException(e);}
+            if (mediaType.equals("image/jpeg")) screenshot.delete();
+        }
+    }
     @AfterClass
-    public static void finalSequence(){new ShutdownSequence().publishReports(System.getProperty("cucumber.filter.tags"));}
+    public static void finalSequence(){
+        String tags = System.getProperty("cucumber.filter.tags");
+        if (tags != null)
+            new ShutdownSequence().publishReports(System.getProperty("cucumber.filter.tags"));
+        else new Printer(TestRunner.class).new Warning("Tags are null!");
+    }
 }
